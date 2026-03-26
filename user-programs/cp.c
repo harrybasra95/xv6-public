3c540cf5a6fa1b6fc9233d6d81f758243b4c6be4
Harnarinder Singh <harry.basra95@gmail.com>
2026-03-26 19:54:44 +0530

    Refactor: Update include paths and improve error handling in cp and cat programs

--- user-programs/cp.c
+++ user-programs/cp.c
@@ -1,3 +1,4 @@
+#include "stat.h"
 #include "user.h"
 #include "fcntl.h"
 
@@ -11,16 +12,31 @@ int main(int argc, char *argv[]) {
   int fd_src = open(argv[1], O_RDONLY);
   if (fd_src < 0) {
     printf(2, "cp: cannot open %s\n", argv[1]);
+    close(fd_src);
     exit();
   }
 
-  int fd_dest = open(argv[2], O_CREATE | O_RDWR);
+  struct stat st;
+
+  if ((fstat(fd_src, &st)) < 0) {
+    printf(2, "cp: cannot stat %s\n", argv[1]);
+    close(fd_src);
+    exit();
+  }
+
+  if (st.type == T_DIR) {
+    printf(2, "cp: %s is not a file\n", argv[1]);
+    close(fd_src);
+    exit();
+  }
+
+  int fd_dest = open(argv[2], O_WRONLY);
   if (fd_dest < 0) {
     printf(2, "cp: cannot create %s\n", argv[2]);
     exit();
   }
 
-  char *fileContent = malloc(512);
+  char fileContent[512];
 
   int n;
   while ((n = read(fd_src, fileContent, 512)) > 0) {
@@ -32,7 +48,6 @@ int main(int argc, char *argv[]) {
     exit();
   }
 
-  free(fileContent);
   close(fd_src);
   close(fd_dest);
 
